34ddb9d8ce2cff56a20b38e5f3df487bc49914a9
mikael <mikael.moreau@freecaster.com>
2021-04-04 20:39:33 +0200

    Create sample project with issue.

--- src/app/item/item-detail.component.ts
+++ src/app/item/item-detail.component.ts
@@ -1,20 +1,9 @@
-import { Component, OnInit } from '@angular/core'
-import { ActivatedRoute } from '@angular/router'
-
-import { Item } from './item'
-import { ItemService } from './item.service'
+import { Component } from '@angular/core'
 
 @Component({
   selector: 'ns-details',
   templateUrl: './item-detail.component.html',
+  // Loading the css seems to be the issue, comment next line and the app will not freeze
+  styleUrls: ['./item-detail.component.css']
 })
-export class ItemDetailComponent implements OnInit {
-  item: Item
-
-  constructor(private itemService: ItemService, private route: ActivatedRoute) {}
-
-  ngOnInit(): void {
-    const id = +this.route.snapshot.params.id
-    this.item = this.itemService.getItem(id)
-  }
-}
+export class ItemDetailComponent {}
--- src/app/item/items.component.ts
+++ src/app/item/items.component.ts
@@ -1,18 +1,9 @@
-import { Component, OnInit } from '@angular/core'
-
-import { Item } from './item'
-import { ItemService } from './item.service'
+import { Component } from '@angular/core'
 
 @Component({
   selector: 'ns-items',
   templateUrl: './items.component.html',
 })
-export class ItemsComponent implements OnInit {
-  items: Array<Item>
-
-  constructor(private itemService: ItemService) {}
-
-  ngOnInit(): void {
-    this.items = this.itemService.getItems()
-  }
+export class ItemsComponent {
+  showDetails: boolean = false;
 }
